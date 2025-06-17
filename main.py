03d774dc110aebb20b9e4cb11700e4e6035e0ce5
default0user <muhammadrafaymg2gmail.com>
2025-06-17 12:35:06 +0500

    End Points

--- main.py
+++ main.py
@@ -5,10 +5,55 @@ class TodoType(BaseModel):
     id:int
     task:str
 
-TODO:list=[]
+TODO:list[TodoType]=[
+    {"id":1,"task":"give me tha phone"}
+]
 
 app=FastAPI()
 
 @app.get("/")
 def root():
     return "hello World"
+
+@app.get("/todo")
+def todo():
+    return TODO
+
+@app.get("/todo/{id}")
+def todo(id:int):
+    for i in range(len(TODO)):
+        if(id==TODO[i]["id"]):
+            return TODO[i]
+    return "no todo found"
+
+@app.post("/todo")
+def todo(todo:TodoType):
+    for i in range(len(TODO)):
+        if todo.id in TODO[i]["id"]:
+            return "id already exist"
+    TODO.append(todo)
+    return todo
+
+@app.delete("/todo/{id}")
+def todo(id:int):
+    for i in range(len(TODO)):
+        if id == TODO[i]["id"]:
+            TODO.pop(i)
+            return "todo deleted..."
+    return "id doesnt exist"
+
+@app.put("/todo/{id}")
+def todo(id:int,todo:TodoType):
+    for i in range(len(TODO)):
+        if id == TODO[i]["id"]:
+            TODO[i]["task"] = todo.task        
+            return todo
+    return "id doesnt exist"
+
+
+
+    
+    
+
+
+
