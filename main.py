c8b410a2e96c05623e754370382dc8c14fdd8cb3
default0user <muhammadrafaymg2gmail.com>
2025-06-17 11:31:21 +0500

    Created Root

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from todo!")
+from fastapi import FastAPI
+from pydantic import BaseModel
 
+class TodoType(BaseModel):
+    id:int
+    task:str
 
-if __name__ == "__main__":
-    main()
+TODO:list=[]
+
+app=FastAPI()
+
+@app.get("/")
+def root():
+    return "hello World"
